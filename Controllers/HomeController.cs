d9e94c80ac7bebb27b8f51057964b7893599571d
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-03-24 00:11:30 -0400

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using HealthcareSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HealthcareSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(SignUpModel model)
+        {
+            addPatientAccount(model);
+            return RedirectToAction("Index");
+        }
+
+
+
+        #region PRIVATE
+
+        private void addPatientAccount(SignUpModel account)
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HealthcareSystem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("HealthcareSystem")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("609d7f7e-c64a-4c18-9697-ca4ae60763d1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
