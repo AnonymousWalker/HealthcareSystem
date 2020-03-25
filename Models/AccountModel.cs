9d807015b40d87303c52c92314a063ce9d013fff
Hoang Anh <hoanganhtran1998@gmail.com>
2020-03-24 23:41:53 -0500

    models created

---
+++ Models/AccountModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthcareSystem.Models
+{
+    public class AccountModel
+    {
+        public int AccountId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string BillingAddress { get; set; }
+        public string Phone { get; set; }
+        public string SSN { get; set; }
+        public double? Salary { get; set; }
+    }
+}
